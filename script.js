6db7d292346825bcfab4d6d808b379df5172a552
Sanjyot Phadatare <104548706+sanjyot002@users.noreply.github.com>
2024-09-08 10:47:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('sign-in-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Simulate a sign-in process
+    window.location.href = 'bill.html'; // Redirect to the bill page after successful sign-in
+});
